3e0aa19da4c221679b2aaf9c0d943cdb4a0013e6
EGY-SuperOne <egy.mohammedmounir@gmail.com>
2021-01-30 01:47:06 +0200

    Added Reviews Model & Service

---
+++ src/app/_model/reviews.ts
@@ -0,0 +1,11 @@
+export interface Review {
+  id?: number;
+  reviewerID?: string;
+  reviewerName?: string;
+  reviewTime?: string;
+  reviewSummary?: string;
+  fullReview?: string;
+  overallRate?: number;
+  reviewVote?: number;
+  productID?: number;
+}
