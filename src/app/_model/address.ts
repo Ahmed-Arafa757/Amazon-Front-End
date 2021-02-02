0cc26dad64e5dae8934e1cc0f3e67fe5cf60bfef
Serety <serety95@gmail.com>
2021-02-03 00:03:05 +0200

    Update address.ts

--- src/app/_model/address.ts
+++ src/app/_model/address.ts
@@ -1,8 +1,10 @@
+import { Location } from "./location";
+
 export interface Address{
     postalCode?: number,
         street?: string,
         state?: string,
         city?: string,
         country?: string,
-        geoMap?:Location 
+        geoMap?:Location
 }
