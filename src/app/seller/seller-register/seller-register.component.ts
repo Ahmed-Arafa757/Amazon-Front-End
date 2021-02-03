834734c71d8bae7ad95939eb05dc855397de089e
Serety <serety95@gmail.com>
2021-02-03 02:27:08 +0200

    sellers register

--- src/app/seller/seller-register/seller-register.component.ts
+++ src/app/seller/seller-register/seller-register.component.ts
@@ -1,4 +1,7 @@
+import { SellersService } from './../../_services/sellers.service';
 import { Component, OnInit } from '@angular/core';
+import { Seller } from 'src/app/_model/sellers';
+import { Form } from '@angular/forms';
 
 @Component({
   selector: 'app-seller-register',
@@ -6,10 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./seller-register.component.scss']
 })
 export class SellerRegisterComponent implements OnInit {
-
-  constructor() { }
+  seller:Seller
+  constructor(private sellersService:SellersService) { }
 
   ngOnInit(): void {
   }
+  onSubmit(form:Form){
+    console.log(form)
+    const newSeller: Seller = {
+     
+      // sellerName: form.sellerName,
+      // address: form.address,
+      
+      // shortDesc: form.shortDesc,
+      // websiteURL: seller.websiteURL,
+      // email: seller.email,
+      // password: seller.password
 
+
+
+  };
+    // console.log(this.sellersService.sellers)
+    // this.sellersService.addSeller(newSeller)
+    // this.sellerAuthService.login(this.seller).subscribe(
+    //   (response)=>{
+    //     localStorage.setItem('token',response['token']);
+    //    console.log(response);
+    //    this.router.navigate(['/product']);
+    //   },
+    //   (err)=>{console.log(err);
+    //   },
+    //   ()=>{}
+    // )
+  }  
 }
