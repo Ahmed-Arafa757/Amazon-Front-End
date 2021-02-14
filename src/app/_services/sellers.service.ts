4bdea2a221e3d6a314e27fda13599d0285c3d068
Arafa <aarafa757@gmail.com>
2021-02-14 14:25:48 +0200

    adding user sign in and fixing backToTop button and fixing sign in butons (colors)

--- src/app/_services/sellers.service.ts
+++ src/app/_services/sellers.service.ts
@@ -1,19 +1,19 @@
 import { Seller } from '../_model/sellers';
 
 export class SellersService {
-    sellers: Seller[] = [
+     sellers: Seller [] = [
         {
             _id: '5ff61753473e1c27b8e6cea1',
             sellerName: 'Schmitt, Ortiz and Jacobi',
             sellerId: '38pozurx_j0noov1f5',
             category: 'Electronics',
-            address:{geoMap: {
+            address: {geoMap: {
                 latitude: -48.7675,
                 longitude: -16.594146387
             }},
             logo: 'https://static.vecteezy.com/system/resources/previews/000/584/922/non_2x/bird-and-wing-logo-vector-template.jpg',
             email: 'Kennith.Gottlieb98@hotmail.com',
-            password:'CrWustxT4S8zHFA',
+            password: 'CrWustxT4S8zHFA',
             shortDesc: 'Schmitt, Ortiz and Jacobi Short Description',
             websiteURL: 'http://SchmittOrtizJacobi.com/'
         }, {
@@ -21,13 +21,13 @@ export class SellersService {
             sellerName: 'Schaden Inc',
             sellerId: 'kqesyl6a_pnv0kbbjl',
             category: 'Furniture',
-            address:{geoMap: {
+            address: {geoMap: {
                 latitude: 16.34575,
                 longitude: -4.959382118
             }},
             logo: 'https://static.vecteezy.com/system/resources/previews/000/578/926/non_2x/bat-black-logo-template-white-background-icons-app-vector.jpg',
             email: 'Denis_Kuvalis@hotmail.com',
-            password:'7uWp3l7PE_MSG4T',
+            password: '7uWp3l7PE_MSG4T',
             shortDesc: 'Schaden Inc Short Description',
             websiteURL: 'http://SchadenInc.com/'
         }, {
@@ -35,13 +35,13 @@ export class SellersService {
             sellerName: 'Armstrong, Gaylord and McGlynn',
             sellerId: 'uoqtjrkf_w37gscwgm',
             category: 'Jewelery',
-            address:{geoMap: {
+            address: {geoMap: {
                 latitude: -163.20023,
                 longitude: 36.048264491
             }},
             logo: 'https://static.vecteezy.com/system/resources/previews/000/579/226/non_2x/vector-business-finance-logo-template.jpg',
             email: 'Alvena52@gmail.com',
-            password:'l7BsqPwrjlqVkaI',
+            password: 'l7BsqPwrjlqVkaI',
             shortDesc: 'Armstrong, Gaylord and McGlynn Short Description',
             websiteURL: 'http://ArmstrongGaylordMcGlynn.com/'
         }, {
@@ -49,13 +49,13 @@ export class SellersService {
             sellerName: 'Schroeder, Lehner and Nitzsche',
             sellerId: '7ztgxkbe_h6xxberh9',
             category: 'Garden',
-            address:{geoMap: {
+            address: {geoMap: {
                 latitude: 176.91495,
                 longitude: 81.18666385
             }},
             logo: 'https://static.vecteezy.com/system/resources/previews/000/612/107/non_2x/star-logo-template-vector-icon-illustration-design.jpg',
             email: 'Filomena68@yahoo.com',
-            password:'5T25l_i4XEkoZAh',
+            password: '5T25l_i4XEkoZAh',
             shortDesc: 'Schroeder, Lehner and Nitzsche Short Description',
             websiteURL: 'http://SchroederLehnerNitzsche.com/'
         }, {
@@ -63,13 +63,13 @@ export class SellersService {
             sellerName: 'Herzog, Zemlak and Klein',
             sellerId: 'xdpd0n7_a5dz7c7yz',
             category: 'Electronics',
-            address:{geoMap: {
+            address: {geoMap: {
                 latitude: 90.19834,
                 longitude: -34.798934745
             }},
             logo: 'https://static.vecteezy.com/system/resources/previews/000/599/214/non_2x/book-reading-logo-and-symbols-template-icons-vector.jpg',
             email: 'Keara75@yahoo.com',
-            password:'V4j0CYDwktZXYxQ',
+            password: 'V4j0CYDwktZXYxQ',
             shortDesc: 'Herzog, Zemlak and Klein Short Description',
             websiteURL: 'http://HerzogZemlakKlein.com/'
         }, {
@@ -77,13 +77,13 @@ export class SellersService {
             sellerName: 'Reinger - Sanford',
             sellerId: 'qpjwxww_ca5880j3s',
             category: 'Health',
-            address:{geoMap: {
+            address: {geoMap: {
                 latitude: -152.17174,
                 longitude: -53.925310674
             }},
             logo: 'https://static.vecteezy.com/system/resources/previews/000/599/252/non_2x/vector-book-reading-logo-and-symbols-template-icons.jpg',
             email: 'Fritz_Schneider@hotmail.com',
-            password:'QyYhTulGtYu9Msq',
+            password: 'QyYhTulGtYu9Msq',
             shortDesc: 'Reinger - Sanford Short Description',
             websiteURL: 'http://ReingerSanford.com/'
         }, {
@@ -91,13 +91,13 @@ export class SellersService {
             sellerName: 'Halvorson - Johnson',
             sellerId: 'w9qbt2be_iphvvk7x9',
             category: 'Books',
-            address:{geoMap: {
+            address: {geoMap: {
                 latitude: 71.7421,
                 longitude: 48.444763804
             }},
             logo: 'https://static.vecteezy.com/system/resources/previews/000/564/007/non_2x/finance-logo-and-symbols-vector-concept.jpg',
             email: 'Eloisa_Hamill@gmail.com',
-            password:'EcIHDjxJkY4ZVdB',
+            password: 'EcIHDjxJkY4ZVdB',
             shortDesc: 'Halvorson - Johnson Short Description',
             websiteURL: 'http://HalvorsonJohnson.com/'
         }, {
@@ -105,13 +105,13 @@ export class SellersService {
             sellerName: 'Will, Daniel and Kuhlman',
             sellerId: 'tc8k8qpt_wi86cd8oz',
             category: 'Toys',
-            address:{geoMap: {
+            address: {geoMap: {
                 latitude: -76.6613,
                 longitude: 6.9000072513
             }},
             logo: 'https://static.vecteezy.com/system/resources/previews/000/563/535/non_2x/finance-logo-and-symbols-vector-concept.jpg',
             email: 'Marianna78@yahoo.com',
-            password:'rqF0XM1c49GBaF7',
+            password: 'rqF0XM1c49GBaF7',
             shortDesc: 'Will, Daniel and Kuhlman Short Description',
             websiteURL: 'http://WillDanielKuhlman.com/'
         }, {
@@ -119,13 +119,13 @@ export class SellersService {
             sellerName: 'Pfannerstill, Waelchi and Raynor',
             sellerId: 'mbrj93jl_2t7cdrprs',
             category: 'Home',
-            address:{geoMap: {
+            address: {geoMap: {
                 latitude: 60.95377,
                 longitude: -29.611926269
             }},
             logo: 'https://static.vecteezy.com/system/resources/previews/000/584/832/non_2x/bird-and-wing-logo-vector-template.jpg',
             email: 'Andreane_Balistreri37@yahoo.com',
-            password:'H5xqvscG0Hh9pOH',
+            password: 'H5xqvscG0Hh9pOH',
             shortDesc: 'Pfannerstill, Waelchi and Raynor Short Description',
             websiteURL: 'http://PfannerstillWaelchiRaynor.com/'
         }, {
@@ -133,13 +133,13 @@ export class SellersService {
             sellerName: 'Mann - Jenkins',
             sellerId: '0cmio9b_2kpjyrqvg',
             category: 'Furniture',
-            address:{geoMap: {
+            address: {geoMap: {
                 latitude: -59.97579,
                 longitude: -53.205117041
             }},
             logo: 'https://static.vecteezy.com/system/resources/previews/000/596/339/non_2x/pyramid-logo-and-symbol-business-abstract-design-template-vector.jpg',
             email: 'Quinten.Hodkiewicz@gmail.com',
-            password:'kjnOGC2VTf4259l',
+            password: 'kjnOGC2VTf4259l',
             shortDesc: 'Mann - Jenkins Short Description',
             websiteURL: 'http://MannJenkins.com/'
         }, {
@@ -147,13 +147,13 @@ export class SellersService {
             sellerName: 'Marquardt, Brakus and Wilderman',
             sellerId: 'yva670rd_xp4zptce0',
             category: 'Health',
-            address:{geoMap: {
+            address: {geoMap: {
                 latitude: 28.199139,
                 longitude: -64.679373317
             }},
             logo: 'https://static.vecteezy.com/system/resources/previews/000/579/628/non_2x/business-finance-logo-template-vector.jpg',
             email: 'Jailyn_Skiles66@hotmail.com',
-            password:'XqHy0CfAjNwk4nb',
+            password: 'XqHy0CfAjNwk4nb',
             shortDesc: 'Marquardt, Brakus and Wilderman Short Description',
             websiteURL: 'http://MarquardtBrakusWilderman.com/'
         }, {
@@ -161,13 +161,13 @@ export class SellersService {
             sellerName: 'Reichel, Brekke and Grady',
             sellerId: '3zevjxnq_ef9bz77u1',
             category: 'Furniture',
-            address:{geoMap: {
+            address: {geoMap: {
                 latitude: 151.43313,
                 longitude: -62.953942434
             }},
             logo: 'https://static.vecteezy.com/system/resources/previews/000/563/839/non_2x/finance-logo-and-symbols-vector-concept.jpg',
             email: 'Lawrence81@hotmail.com',
-            password:'3yXZsJxQ47xP1a2',
+            password: '3yXZsJxQ47xP1a2',
             shortDesc: 'Reichel, Brekke and Grady Short Description',
             websiteURL: 'http://ReichelBrekkeGrady.com/'
         }, {
@@ -175,13 +175,13 @@ export class SellersService {
             sellerName: 'Graham, Pacocha and Paucek',
             sellerId: 'ps2vkjdm_1e11gaoeh',
             category: 'Games',
-            address:{geoMap: {
+            address: {geoMap: {
                 latitude: 27.710009,
                 longitude: -8.738150011
             }},
             logo: 'https://static.vecteezy.com/system/resources/previews/000/585/013/non_2x/bird-and-wing-logo-vector-template.jpg',
             email: 'Walker.Klein@yahoo.com',
-            password:'O3ou0o0D_N83eaH',
+            password: 'O3ou0o0D_N83eaH',
             shortDesc: 'Graham, Pacocha and Paucek Short Description',
             websiteURL: 'http://GrahamPacochaPaucek.com/'
         }, {
@@ -189,13 +189,13 @@ export class SellersService {
             sellerName: 'Heller, Larson and Kuhic',
             sellerId: 'l25xq1k8_lu23vsed6',
             category: 'Home',
-            address:{geoMap: {
+            address: {geoMap: {
                 latitude: -106.7931,
                 longitude: -35.209501957
             }},
             logo: 'https://static.vecteezy.com/system/resources/previews/000/612/445/non_2x/star-logo-template-vector-icon-illustration-design.jpg',
             email: 'Delfina_Rutherford@yahoo.com',
-            password:'HQeduvcUuZm1px2',
+            password: 'HQeduvcUuZm1px2',
             shortDesc: 'Heller, Larson and Kuhic Short Description',
             websiteURL: 'http://HellerLarsonKuhic.com/'
         }, {
@@ -203,13 +203,13 @@ export class SellersService {
             sellerName: 'Monahan, Thompson and Walter',
             sellerId: '9wk4ceeu_w2onuga1x',
             category: 'Electronics',
-            address:{geoMap: {
+            address: {geoMap: {
                 latitude: 149.54541,
                 longitude: -73.360648364
             }},
             logo: 'https://static.vecteezy.com/system/resources/previews/000/579/056/non_2x/business-finance-logo-template-vector.jpg',
             email: 'Justina10@hotmail.com',
-            password:'HL1n9SvATj8oLLm',
+            password: 'HL1n9SvATj8oLLm',
             shortDesc: 'Monahan, Thompson and Walter Short Description',
             websiteURL: 'http://MonahanThompsonWalter.com/'
         }, {
@@ -217,13 +217,13 @@ export class SellersService {
             sellerName: 'Macejkovic Inc',
             sellerId: 'ahthtrp_ufh6iw3oh',
             category: 'Music',
-            address:{geoMap: {
+            address: {geoMap: {
                 latitude: -145.74749,
                 longitude: 87.84537932
             }},
             logo: 'https://static.vecteezy.com/system/resources/previews/000/585/554/non_2x/bird-and-wing-logo-vector-template.jpg',
             email: 'Jairo41@hotmail.com',
-            password:'XR6ZkhfMeT_7qY6',
+            password: 'XR6ZkhfMeT_7qY6',
             shortDesc: 'Macejkovic Inc Short Description',
             websiteURL: 'http://MacejkovicInc.com/'
         }, {
@@ -231,13 +231,13 @@ export class SellersService {
             sellerName: 'Wilkinson Inc',
             sellerId: 'pwmw300b_c603i9oem',
             category: 'Industrial',
-            address:{geoMap: {
+            address: {geoMap: {
                 latitude: -30.77997,
                 longitude: -20.647845898
             }},
             logo: 'https://static.vecteezy.com/system/resources/previews/000/579/011/non_2x/star-logo-template-vector-icon-illustration-design.jpg',
             email: 'Stephania.Heathcote@hotmail.com',
-            password:'DJLOdiOwYUEQx6c',
+            password: 'DJLOdiOwYUEQx6c',
             shortDesc: 'Wilkinson Inc Short Description',
             websiteURL: 'http://WilkinsonInc.com/'
         }, {
@@ -245,19 +245,19 @@ export class SellersService {
             sellerName: 'Nienow and Sons',
             sellerId: 'p0fs5uzs_1lfbovu6u',
             category: 'Furniture',
-            address:{geoMap: {
+            address: {geoMap: {
                 latitude: 148.61019,
                 longitude: 20.394189738
             }},
             logo: 'https://static.vecteezy.com/system/resources/previews/000/599/745/non_2x/vector-business-corporate-s-letter-logo.jpg',
             email: 'Harmony.Senger@hotmail.com',
-            password:'Gxh_hWjgCH46F8p',
+            password: 'Gxh_hWjgCH46F8p',
             shortDesc: 'Nienow and Sons Short Description',
             websiteURL: 'http://NienowSons.com/'
         }
     ];
 
-    constructor() { }
+    constructor() { } 
 
 
 
