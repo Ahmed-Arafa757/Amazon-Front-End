80180320f595a3f417c7ddc9f08bf5790af3e19d
Serety <serety95@gmail.com>
2021-02-24 21:02:21 +0200

    sellers auth

--- src/app/_services/seller-auth.service.ts
+++ src/app/_services/seller-auth.service.ts
@@ -5,29 +5,8 @@ import { Seller } from '../_model/sellers';
   providedIn: 'root'
 })
 export class SellerAuthService {
-  sellers: Seller[] = [
-    {
-      sellerId: '5ff888aaa3f4d75244531911',
-      sellerName: 'Serety95',
-      email: 'serety@test.com',
-      password: 'seretytest',
-      repeatedPassword: 'seretytest'
-    },
-    {
-      sellerId: '5ff888aaa3f4d75244532211',
-      sellerName: 'tobshy',
-      email: 'tobshy@test.com',
-      password: 'tobshytest',
-      repeatedPassword: 'tobshytest'
-    },
-    {
-      sellerId: '5ff888aaae85d929b7aa01c0',
-      sellerName: 'AmrHossam',
-      email: 'AmrHossam@test.com',
-      password: 'AmrHossamtest',
-      repeatedPassword: 'AmrHossamtest'
-    }
-  ];
+  sellers: Seller[] = []
+    
 
 
   baseUrl = 'https://mearn-stack-backend-test.herokuapp.com/';
