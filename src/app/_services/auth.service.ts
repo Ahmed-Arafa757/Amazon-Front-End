821a650e92051876056c2cccfdf6442bbf240ac2
EGY-SuperOne <egy.mohammedmounir@gmail.com>
2021-03-06 04:23:55 +0200

    Changed auth service backend to heroku

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -4,31 +4,28 @@ import { Person } from '../_model/person';
 import { User } from '../_model/users';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-  baseUrl = 'http://localhost:3000/';
+  // baseUrl = 'http://localhost:3000/';
+  baseUrl = 'https://iti-amzon-backend.herokuapp.com/';
 
   // userLoggedIn = new EventEmitter<User>();
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
   register(user: User) {
     return this.httpClient.post(`${this.baseUrl}user/register`, user);
-
   }
 
-  login(user:User) {
-    
+  login(user: User) {
     return this.httpClient.post(`${this.baseUrl}user/login`, user);
   }
 
   isAuthenticated(): boolean {
-
     if (localStorage.getItem('token')) {
       return true;
-    }
-    else {
+    } else {
       return false;
     }
   }
