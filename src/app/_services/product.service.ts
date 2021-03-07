2aeeb62e0f60c31450997458990bf6ff183a73be
amrhossam97 <amrhossam60@hotmail.com>
2021-03-07 02:06:19 +0200

    fix

--- src/app/_services/product.service.ts
+++ src/app/_services/product.service.ts
@@ -9,8 +9,8 @@ export class ProductService {
   products: Product[];
 
   cartProducts = [];
-  /* baseUrl = 'https://iti-amzon-backend.herokuapp.com/'; */
- baseUrl = 'http://localhost:3000/';
+  baseUrl = 'https://iti-amzon-backend.herokuapp.com/';
+/*  baseUrl = 'http://localhost:3000/'; */
   productAdded = new EventEmitter<Product[]>();
 
   constructor(private httpClinet: HttpClient) {}
