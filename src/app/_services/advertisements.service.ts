f019a4b9cf76de3d29edfb7c7db2f29d165d8d59
amrhossam97 <amrhossam60@hotmail.com>
2021-03-07 10:59:23 +0200

    finish

--- src/app/_services/advertisements.service.ts
+++ src/app/_services/advertisements.service.ts
@@ -155,7 +155,7 @@ export class AdvertisementsService {
     advertisements;
 
 
-    baseUrl = 'http://localhost:3000/';
+    baseUrl = 'https://iti-amzon-backend.herokuapp.com/';
 
     constructor(private httpClient: HttpClient) { }
 
